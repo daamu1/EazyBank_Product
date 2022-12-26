9989c1c63c7ea47fda173fe15d5a11911c1a2a68
shady <saurabhmaithani01921@gmail.com>
2022-12-26 15:51:54 +0530

    Every Thing is Totally Upgraded

--- EazyaBankBackEndApplication/src/main/java/com/eazybytes/controller/ContactController.java
+++ EazyaBankBackEndApplication/src/main/java/com/eazybytes/controller/ContactController.java
@@ -1,15 +1,17 @@
 package com.eazybytes.controller;
 
-import java.sql.Date;
-import java.util.Random;
-
+import com.eazybytes.model.Contact;
+import com.eazybytes.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eazybytes.model.Contact;
-import com.eazybytes.repository.ContactRepository;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 public class ContactController {
@@ -18,10 +20,16 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    /*@PreFilter("filterObject.contactName != 'Test'")*/
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        List<Contact> returnContacts = new ArrayList();
+        returnContacts.add(contact);
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
