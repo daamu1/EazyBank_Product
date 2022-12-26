42e3cd6a0aad724b753bc8268ee3c31ddc10d272
shady <saurabhmaithani01921@gmail.com>
2022-12-26 17:24:12 +0530

    Final commit from our side

---
+++ EazyaBankBackEndApplication/src/main/java/com/eazybytes/config/RabbitMQConfiguration.java
@@ -0,0 +1,108 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class RabbitMQConfiguration {
+    public static final String topicExchangeName="message_queue_exchange";
+    public static final String queueName="message_queue";
+
+    @Bean
+    public Queue queue()
+    {
+        return new Queue() {
+            @Override
+            public boolean add(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean offer(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object remove() {
+                return null;
+            }
+
+            @Override
+            public Object poll() {
+                return null;
+            }
+
+            @Override
+            public Object element() {
+                return null;
+            }
+
+            @Override
+            public Object peek() {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public Object[] toArray(Object[] a) {
+                return new Object[0];
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+        };
+    }
+}
